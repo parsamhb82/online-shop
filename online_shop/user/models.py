fcd3129a4f601f6768375edce4b56e6295b12f53
parsamhb82 <parsa1382mohebian@gmail.com>
2024-08-28 18:52:15 +0330

    ....

--- online_shop/user/models.py
+++ online_shop/user/models.py
@@ -5,7 +5,7 @@ class Costumer(models.Model):
     last_name = models.CharField(max_length=100, null=False, blank=False)
     email = models.EmailField(max_length=100, null=True, blank=True, unique=True)
     username = models.CharField(max_length=36, help_text='username must be unique and less than 36 characters', unique=True, blank=False, null=False)
-    password = models.CharField(max_length=36, help_text='password must be less than 36 characters', blank=False, null=False)  
+    password = models.CharField(max_length=255, help_text='password must be less than 36 characters', blank=False, null=False)  
     address = models.TextField(null=True, blank=True)
     city = models.CharField(max_length=100, null=True, blank=True)
     phone_number = models.CharField(max_length=100, null=False, blank=False, unique=True)
